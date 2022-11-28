abaa9de72ebc2f4572113b8f93a22fd95355967d
NicolasBissig <nicbissig@gmail.com>
2022-11-28 16:53:21 +0100

    test!: add first test, refactor @HttpFunction

--- src/decorators/http/http-function.ts
+++ src/decorators/http/http-function.ts
@@ -4,41 +4,43 @@ import { BodyMetaDataKey } from './request-body';
 import { QueryDescriptor, QueryMetaDataKey } from './query-parameter';
 import { PathParameterDescriptor, PathParameterMetaDataKey } from './path-parameter';
 
-export function HttpFunction(target: any, propertyName: string, descriptor: TypedPropertyDescriptor<Function>) {
-    let method = descriptor.value!;
+export function HttpFunction(): MethodDecorator {
+    return (target: any, propertyName: string | symbol, descriptor: TypedPropertyDescriptor<any>) => {
+        let method: Function = descriptor.value!;
 
-    descriptor.value = function(...args: any[]) {
-        const context: Context = args[0];
-        const req = context.req!;
+        descriptor.value = function(...args: any[]) {
+            const context: Context = args[0];
+            const req = context.req!;
 
-        let bodyParameter: number[] = Reflect.getOwnMetadata(BodyMetaDataKey, target, propertyName);
-        if (bodyParameter) {
-            if (bodyParameter.length !== 1) {
-                throw new Error('only one @RequestBody parameter is allowed');
+            let bodyParameter: number[] = Reflect.getOwnMetadata(BodyMetaDataKey, target, propertyName);
+            if (bodyParameter) {
+                if (bodyParameter.length !== 1) {
+                    throw new Error('only one @RequestBody parameter is allowed');
+                }
+
+                const paramIndex = bodyParameter[0];
+                args[paramIndex] = JSON.parse(req.rawBody);
+            }
+            let queryParameters: QueryDescriptor[] = Reflect.getOwnMetadata(QueryMetaDataKey, target, propertyName);
+
+            if (queryParameters) {
+                for (let parameter of queryParameters) {
+                    args[parameter.index] = req.query[parameter.name];
+                }
             }
 
-            const paramIndex = bodyParameter[0];
-            args[paramIndex] = JSON.parse(req.rawBody);
-        }
-        let queryParameters: QueryDescriptor[] = Reflect.getOwnMetadata(QueryMetaDataKey, target, propertyName);
-
-        if (queryParameters) {
-            for (let parameter of queryParameters) {
-                args[parameter.index] = req.query[parameter.name];
+            let pathParameters: PathParameterDescriptor[] = Reflect.getOwnMetadata(
+                PathParameterMetaDataKey,
+                target,
+                propertyName
+            );
+            if (pathParameters) {
+                for (let parameter of pathParameters) {
+                    args[parameter.index] = req.params[parameter.name];
+                }
             }
-        }
 
-        let pathParameters: PathParameterDescriptor[] = Reflect.getOwnMetadata(
-            PathParameterMetaDataKey,
-            target,
-            propertyName
-        );
-        if (pathParameters) {
-            for (let parameter of pathParameters) {
-                args[parameter.index] = req.params[parameter.name];
-            }
-        }
-
-        return method.apply(this, args);
+            return method.apply(this, args);
+        };
     };
 }
---
+++ test/decorators/http/http-function.test.ts
@@ -0,0 +1,46 @@
+import { Context, HttpRequest, HttpResponse } from '@azure/functions';
+import { HttpFunction, RequestBody, QueryParameter } from '../../../src';
+
+type body = {
+    id: number;
+};
+
+type echoResponse = {
+    body: body;
+    queryParameter: Record<string, string>;
+};
+
+class Echo {
+    @HttpFunction()
+    static async httpTrigger(
+        _: Context,
+        @RequestBody body: body,
+        @QueryParameter('query') query: string
+    ): Promise<HttpResponse> {
+        return {
+            body: {
+                body: body,
+                queryParameter: { query: query },
+            } as echoResponse,
+        };
+    }
+}
+
+describe('HTTP function decorators', () => {
+    it('works', async () => {
+        const body: body = { id: 42 };
+
+        const context = ({
+            req: ({
+                rawBody: JSON.stringify(body),
+                query: {
+                    query: 'queryValue',
+                },
+            } as unknown) as HttpRequest,
+        } as unknown) as Context;
+
+        // @ts-ignore
+        const result = await Echo.httpTrigger(context);
+        expect(result.body.body).toEqual(body);
+    });
+});
