d714200c4b08570d44d1db5c6c98e18d11314f8f
NicolasBissig <nicbissig@gmail.com>
2022-11-29 10:08:10 +0100

    fix: export @PathParameter

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { HttpFunction } from './decorators/http/http-function';
 
 export { RequestBody } from './decorators/http/request-body';
 export { QueryParameter } from './decorators/http/query-parameter';
+export { PathParameter } from './decorators/http/path-parameter';
