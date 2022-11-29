90a487e96d67904acf0411ab04a3049cae1b16ac
NicolasBissig <nicbissig@gmail.com>
2022-11-29 12:40:44 +0100

    refactor: extract handling logic and add type checks

--- src/decorators/http/query-parameter.ts
+++ src/decorators/http/query-parameter.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import { HttpRequest } from '@azure/functions';
 
 export const QueryMetaDataKey = Symbol('QueryParameter');
 export type QueryDescriptor = {
@@ -13,4 +14,14 @@ export function QueryParameter(queryName?: string): ParameterDecorator {
         existingQueryParameters.push({ index: parameterIndex, name: queryName || propertyKey.toString() });
         Reflect.defineMetadata(QueryMetaDataKey, existingQueryParameters, target, propertyKey);
     };
+}
+
+export function handleQueryParameters(target: Object, propertyName: string | symbol, req: HttpRequest, args: any[]) {
+    let queryParameters: QueryDescriptor[] = Reflect.getOwnMetadata(QueryMetaDataKey, target, propertyName);
+
+    if (queryParameters) {
+        for (let parameter of queryParameters) {
+            args[parameter.index] = req.query[parameter.name];
+        }
+    }
 }
