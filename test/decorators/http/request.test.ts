e2d9b41d7c4c0de8ee7a75d82a3ce557e2912a07
NicolasBissig <nicbissig@gmail.com>
2022-11-30 16:30:37 +0100

    test: add test for @Request

--- test/decorators/http/request.test.ts
+++ test/decorators/http/request.test.ts
@@ -3,15 +3,15 @@ import { HttpFunction, Request } from '../../../src';
 import { createContextWithHttpRequest } from './context';
 import { callAzureFunction } from '../azure-function';
 
-class Echo {
-    @HttpFunction()
-    static async httpTrigger(@Request() request: HttpRequest): Promise<HttpRequest> {
-        return request;
-    }
-}
-
 describe('@Request decorator', () => {
     it('passes the request correctly as single argument', async () => {
+        class Echo {
+            @HttpFunction()
+            static async httpTrigger(@Request() request: HttpRequest): Promise<HttpRequest> {
+                return request;
+            }
+        }
+
         const context = createContextWithHttpRequest();
 
         const result = await callAzureFunction(Echo.httpTrigger, context);
