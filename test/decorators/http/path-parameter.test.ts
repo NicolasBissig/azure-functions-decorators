cba395adadd5e8b3fbcde41f1c083c6de8f1e98b
NicolasBissig <nicbissig@gmail.com>
2022-11-30 16:35:14 +0100

    test: add simple tests for @PathParameter

---
+++ test/decorators/http/path-parameter.test.ts
@@ -0,0 +1,43 @@
+import { HttpFunction, PathParameter } from '../../../src';
+import { createContextWithHttpRequest } from './context';
+import { callAzureFunction } from '../azure-function';
+
+describe('@PathParameter decorator', () => {
+    it('passes the path parameter correctly as single argument', async () => {
+        class Echo {
+            @HttpFunction()
+            static async httpTrigger(@PathParameter() page: string): Promise<string> {
+                return page;
+            }
+        }
+
+        const page = '42';
+        const context = createContextWithHttpRequest({
+            params: {
+                page: page,
+            },
+        });
+
+        const result = await callAzureFunction(Echo.httpTrigger, context);
+        expect(result).toEqual(page);
+    });
+
+    it('passes the path parameter correctly as single named argument', async () => {
+        class Echo {
+            @HttpFunction()
+            static async httpTrigger(@PathParameter('ID') id: string): Promise<string> {
+                return id;
+            }
+        }
+
+        const id = 'user-id';
+        const context = createContextWithHttpRequest({
+            params: {
+                ID: id,
+            },
+        });
+
+        const result = await callAzureFunction(Echo.httpTrigger, context);
+        expect(result).toEqual(id);
+    });
+});
