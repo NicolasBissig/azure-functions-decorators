433ee4c6045828dbdaa4f6390d96cbc5d5a07415
NicolasBissig <nicbissig@gmail.com>
2022-11-30 16:51:32 +0100

    refactor!: fix errors in @QueryParameter

--- src/decorators/http/query-parameter.ts
+++ src/decorators/http/query-parameter.ts
@@ -7,11 +7,18 @@ type QueryDescriptor = {
     name: string;
 };
 
-export function QueryParameter(queryName?: string): ParameterDecorator {
+export function QueryParameter(key: string): ParameterDecorator {
     return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
-        const markedParameter = { index: parameterIndex, name: queryName || propertyKey.toString() };
+        const markedParameter = { index: parameterIndex, name: key };
         markParameterWithValue<QueryDescriptor>(target, propertyKey, QueryMetaDataKey, markedParameter);
     };
+}
+
+function findParameter(req: HttpRequest, parameter: string): string | undefined {
+    const query = req.query;
+    if (!query) return undefined;
+    const value = query[parameter];
+    return value ? value : undefined;
 }
 
 export function handleQueryParameters(target: Object, propertyName: string | symbol, req: HttpRequest, args: any[]) {
@@ -19,6 +26,6 @@ export function handleQueryParameters(target: Object, propertyName: string | sym
         target,
         propertyName,
         QueryMetaDataKey,
-        parameter => (args[parameter.index] = req.query[parameter.name])
+        parameter => (args[parameter.index] = findParameter(req, parameter.name))
     );
 }
