76fe2ff84bc5585de288a179e78e170a66a159b2
NicolasBissig <nicbissig@gmail.com>
2022-12-02 11:10:11 +0100

    feat!: return undefined as body if not parseable

--- src/decorators/http/request-body.ts
+++ src/decorators/http/request-body.ts
@@ -9,16 +9,19 @@ export function RequestBody(): ParameterDecorator {
     };
 }
 
+function parseBody(req: HttpRequest): any {
+    try {
+        return JSON.parse(req.rawBody);
+    } catch {
+        return undefined;
+    }
+}
+
 export function handleRequestBodyParameter(
     target: Object,
     propertyName: string | symbol,
     req: HttpRequest,
     args: any[]
 ) {
-    applyToMarked<number>(
-        target,
-        propertyName,
-        BodyMetaDataKey,
-        parameter => (args[parameter] = JSON.parse(req.rawBody))
-    );
+    applyToMarked<number>(target, propertyName, BodyMetaDataKey, parameter => (args[parameter] = parseBody(req)));
 }
