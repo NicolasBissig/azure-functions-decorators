082a17eedeea23a82c59d1f701d2c635a4bfb26d
Leonid Koftun <leonid.koftun@device-insight.com>
2023-01-20 12:53:31 +0100

    fix: set default statusCode to 200

--- packages/decorators/src/decorators/http/request-mapping.ts
+++ packages/decorators/src/decorators/http/request-mapping.ts
@@ -1,13 +1,13 @@
-import { isContext, isFunction, isHttpRequest, isHttpResponse } from './type-guards';
-import { handleContextParameter } from '../context';
-import { handleRequestParameter } from './http-request';
-import { handleRequestBodyParameter } from './request-body';
-import { handleQueryParameters } from './query-parameter';
-import { handlePathParameter } from './path-parameter';
-import { handleError } from './http-status';
-import { parsePathWithParameters, pathWithParametersToRegex, toValidPath } from './parameters';
-import { registerMapping } from './rest-controller';
-import { HttpMethod, HttpResponse } from '@azure/functions';
+import {isContext, isFunction, isHttpRequest, isHttpResponse} from './type-guards';
+import {handleContextParameter} from '../context';
+import {handleRequestParameter} from './http-request';
+import {handleRequestBodyParameter} from './request-body';
+import {handleQueryParameters} from './query-parameter';
+import {handlePathParameter} from './path-parameter';
+import {handleError} from './http-status';
+import {parsePathWithParameters, pathWithParametersToRegex, toValidPath} from './parameters';
+import {registerMapping} from './rest-controller';
+import {HttpMethod, HttpResponse} from '@azure/functions';
 
 type ResultMapper<T> = (result: T) => HttpResponse;
 
@@ -21,7 +21,7 @@ type RequestMappingOptions = Partial<FullRequestMappingOptions>;
 const defaultResultMapper: ResultMapper<unknown> = (result: unknown): HttpResponse => {
     if (result === undefined || result === null) {
         // is undefined, return empty success response
-        return { status: 204, statusCode: 204 };
+        return {status: 204, statusCode: 204};
     }
 
     if (isHttpResponse(result)) {
@@ -33,12 +33,16 @@ const defaultResultMapper: ResultMapper<unknown> = (result: unknown): HttpRespon
         // is an object, serialize it in the body
         return {
             body: JSON.stringify(result),
+            status: 200,
+            statusCode: 200,
         };
     }
 
     return {
         // is probably a primitive, return it as body
         body: result,
+        status: 200,
+        statusCode: 200
     };
 };
 
@@ -135,61 +139,61 @@ export function RequestMapping(path?: string, options?: RequestMappingOptions): 
  * GET only version of {@link RequestMapping @RequestMapping}
  */
 export function GetMapping(path?: string, options?: Omit<RequestMappingOptions, 'methods'>): MethodDecorator {
-    return RequestMapping(path, { ...options, methods: ['GET'] });
+    return RequestMapping(path, {...options, methods: ['GET']});
 }
 
 /**
  * POST only version of {@link RequestMapping @RequestMapping}
  */
 export function PostMapping(path?: string, options?: Omit<RequestMappingOptions, 'methods'>): MethodDecorator {
-    return RequestMapping(path, { ...options, methods: ['POST'] });
+    return RequestMapping(path, {...options, methods: ['POST']});
 }
 
 /**
  * DELETE only version of {@link RequestMapping @RequestMapping}
  */
 export function DeleteMapping(path?: string, options?: Omit<RequestMappingOptions, 'methods'>): MethodDecorator {
-    return RequestMapping(path, { ...options, methods: ['DELETE'] });
+    return RequestMapping(path, {...options, methods: ['DELETE']});
 }
 
 /**
  * HEAD only version of {@link RequestMapping @RequestMapping}
  */
 export function HeadMapping(path?: string, options?: Omit<RequestMappingOptions, 'methods'>): MethodDecorator {
-    return RequestMapping(path, { ...options, methods: ['HEAD'] });
+    return RequestMapping(path, {...options, methods: ['HEAD']});
 }
 
 /**
  * PATCH only version of {@link RequestMapping @RequestMapping}
  */
 export function PatchMapping(path?: string, options?: Omit<RequestMappingOptions, 'methods'>): MethodDecorator {
-    return RequestMapping(path, { ...options, methods: ['PATCH'] });
+    return RequestMapping(path, {...options, methods: ['PATCH']});
 }
 
 /**
  * PUT only version of {@link RequestMapping @RequestMapping}
  */
 export function PutMapping(path?: string, options?: Omit<RequestMappingOptions, 'methods'>): MethodDecorator {
-    return RequestMapping(path, { ...options, methods: ['PUT'] });
+    return RequestMapping(path, {...options, methods: ['PUT']});
 }
 
 /**
  * OPTIONS only version of {@link RequestMapping @RequestMapping}
  */
 export function OptionsMapping(path?: string, options?: Omit<RequestMappingOptions, 'methods'>): MethodDecorator {
-    return RequestMapping(path, { ...options, methods: ['OPTIONS'] });
+    return RequestMapping(path, {...options, methods: ['OPTIONS']});
 }
 
 /**
  * TRACE only version of {@link RequestMapping @RequestMapping}
  */
 export function TraceMapping(path?: string, options?: Omit<RequestMappingOptions, 'methods'>): MethodDecorator {
-    return RequestMapping(path, { ...options, methods: ['TRACE'] });
+    return RequestMapping(path, {...options, methods: ['TRACE']});
 }
 
 /**
  * CONNECT only version of {@link RequestMapping @RequestMapping}
  */
 export function ConnectMapping(path?: string, options?: Omit<RequestMappingOptions, 'methods'>): MethodDecorator {
-    return RequestMapping(path, { ...options, methods: ['CONNECT'] });
+    return RequestMapping(path, {...options, methods: ['CONNECT']});
 }
